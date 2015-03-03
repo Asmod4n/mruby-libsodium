89517e6a3cc23cc0c032350cc6b12fb938dd9013
Asmod4n <Asmod4n@users.noreply.github.com>
2015-03-03 17:43:59 +0000

    added aead

--- src/mrb_libsodium.c
+++ src/mrb_libsodium.c
@@ -195,23 +195,20 @@ mrb_crypto_secretbox_easy(mrb_state *mrb, mrb_value self)
   mrb_sodium_check_length(mrb, nonce, crypto_secretbox_NONCEBYTES, "nonce");
   mrb_sodium_check_length(mrb, key_obj, crypto_secretbox_KEYBYTES, "key");
 
-  char *key;
+  const char *key;
 
   switch(mrb_type(key_obj)) {
-    case MRB_TT_STRING:
-      key = RSTRING_PTR(key_obj);
-      break;
     case MRB_TT_DATA:
       key = DATA_PTR(key_obj);
       break;
     default:
-      mrb_raise(mrb, E_TYPE_ERROR, "key must be a String or Data Type");
+      key = mrb_string_value_ptr(mrb, key_obj);
   }
 
   mrb_value ciphertext = mrb_str_buf_new(mrb, crypto_secretbox_MACBYTES + message_len);
   mrb_str_modify(mrb, RSTRING(ciphertext));
   crypto_secretbox_easy((unsigned char *) RSTRING_PTR(ciphertext),
-    (const unsigned char *) message, (size_t) message_len,
+    (const unsigned char *) message, (unsigned long long) message_len,
     (const unsigned char *) RSTRING_PTR(nonce),
     (const unsigned char *) key);
   mrb_str_resize(mrb, ciphertext, crypto_secretbox_MACBYTES + message_len);
@@ -230,22 +227,19 @@ mrb_crypto_secretbox_open_easy(mrb_state *mrb, mrb_value self)
   mrb_sodium_check_length(mrb, nonce, crypto_secretbox_NONCEBYTES, "nonce");
   mrb_sodium_check_length(mrb, key_obj, crypto_secretbox_KEYBYTES, "key");
 
-  char *key;
+  const char *key;
 
   switch(mrb_type(key_obj)) {
-    case MRB_TT_STRING:
-      key = RSTRING_PTR(key_obj);
-      break;
     case MRB_TT_DATA:
       key = DATA_PTR(key_obj);
       break;
     default:
-      mrb_raise(mrb, E_TYPE_ERROR, "key must be a String or Data Type");
+      key = mrb_string_value_ptr(mrb, key_obj);
   }
 
   unsigned char decrypted[ciphertext_len - crypto_secretbox_MACBYTES];
   int rc = crypto_secretbox_open_easy(decrypted,
-    (const unsigned char *) ciphertext, (size_t) ciphertext_len,
+    (const unsigned char *) ciphertext, (unsigned long long) ciphertext_len,
     (const unsigned char *) RSTRING_PTR(nonce),
     (const unsigned char *) key);
 
@@ -255,9 +249,111 @@ mrb_crypto_secretbox_open_easy(mrb_state *mrb, mrb_value self)
   return mrb_str_new(mrb, (char *) decrypted, sizeof(decrypted));
 }
 
+static mrb_value
+mrb_crypto_auth(mrb_state *mrb, mrb_value self)
+{
+  char *message;
+  mrb_int message_len;
+  mrb_value key_obj;
+
+  mrb_get_args(mrb, "so", &message, &message_len, &key_obj);
+
+  mrb_sodium_check_length(mrb, key_obj, crypto_auth_KEYBYTES, "key");
+
+  const char *key;
+
+  switch(mrb_type(key_obj)) {
+    case MRB_TT_DATA:
+      key = DATA_PTR(key_obj);
+      break;
+    default:
+      key = mrb_string_value_ptr(mrb, key_obj);
+  }
+
+  mrb_value mac = mrb_str_buf_new(mrb, crypto_auth_BYTES);
+  mrb_str_modify(mrb, RSTRING(mac));
+  crypto_auth((unsigned char *) RSTRING_PTR(mac),
+    (const unsigned char *) message, (unsigned long long) message_len,
+    (const unsigned char *) key);
+  mrb_str_resize(mrb, mac, crypto_auth_BYTES);
+
+  return mac;
+}
+
+static mrb_value
+mrb_crypto_auth_verify(mrb_state *mrb, mrb_value self)
+{
+  mrb_value mac;
+  char *message;
+  mrb_int message_len;
+  mrb_value key_obj;
+
+  mrb_get_args(mrb, "Sso", &mac, &message, &message_len, &key_obj);
+
+  mrb_sodium_check_length(mrb, mac, crypto_auth_BYTES, "mac");
+  mrb_sodium_check_length(mrb, key_obj, crypto_auth_KEYBYTES, "key");
+
+  const char *key;
+
+  switch(mrb_type(key_obj)) {
+    case MRB_TT_DATA:
+      key = DATA_PTR(key_obj);
+      break;
+    default:
+      key = mrb_string_value_ptr(mrb, key_obj);
+  }
+
+  int rc = crypto_auth_verify((const unsigned char *) RSTRING_PTR(mac),
+    (const unsigned char *) message, (unsigned long long) message_len,
+    (const unsigned char *) key);
+
+  switch(rc) {
+    case -1:
+      return mrb_false_value();
+    case 0:
+      return mrb_true_value();
+    default:
+      mrb_raisef(mrb, E_SODIUM_ERROR, "crypto_auth_verify returned erroneous value %S", mrb_fixnum_value(rc));
+  }
+}
+
+static mrb_value
+mrb_crypto_aead_chacha20poly1305_encrypt(mrb_state *mrb, mrb_value self)
+{
+  char *message, *additional_data = NULL;
+  mrb_int message_len, additional_data_len = 0;
+  mrb_value nonce, key_obj;
+
+  mrb_get_args(mrb, "sSo|s", &message, &message_len, &nonce, &key_obj, &additional_data, &additional_data_len);
+
+  mrb_sodium_check_length(mrb, nonce, crypto_aead_chacha20poly1305_NPUBBYTES, "nonce");
+  mrb_sodium_check_length(mrb, key_obj, crypto_aead_chacha20poly1305_KEYBYTES, "key");
+
+  const char *key;
+
+  switch(mrb_type(key_obj)) {
+    case MRB_TT_DATA:
+      key = DATA_PTR(key_obj);
+      break;
+    default:
+      key = mrb_string_value_ptr(mrb, key_obj);
+  }
+
+  unsigned char ciphertext[message_len + crypto_aead_chacha20poly1305_ABYTES];
+  unsigned long long ciphertext_len;
+
+  crypto_aead_chacha20poly1305_encrypt(ciphertext, &ciphertext_len,
+    (const unsigned char *) message, (unsigned long long) message_len,
+    (const unsigned char *) additional_data, (unsigned long long) additional_data_len,
+    NULL, (const unsigned char *) RSTRING_PTR(nonce),
+    (const unsigned char *) key);
+
+  return mrb_str_new(mrb, (const char *) ciphertext, (size_t) ciphertext_len);
+}
+
 void
 mrb_mruby_libsodium_gem_init(mrb_state* mrb) {
-  struct RClass *sodium_mod, *secure_buffer_cl, *randombytes_mod, *crypto_mod;
+  struct RClass *sodium_mod, *secure_buffer_cl, *randombytes_mod, *crypto_mod, *crypto_aead_mod, *crypto_aead_chacha20poly1305_mod;
 
   sodium_mod = mrb_define_module(mrb, "Sodium");
   mrb_define_class_under(mrb, sodium_mod, "Error", E_RUNTIME_ERROR);
@@ -280,6 +376,12 @@ mrb_mruby_libsodium_gem_init(mrb_state* mrb) {
   crypto_mod = mrb_define_module(mrb, "Crypto");
   mrb_define_module_function(mrb, crypto_mod, "secretbox",      mrb_crypto_secretbox_easy,      MRB_ARGS_REQ(2));
   mrb_define_module_function(mrb, crypto_mod, "secretbox_open", mrb_crypto_secretbox_open_easy, MRB_ARGS_REQ(2));
+  mrb_define_module_function(mrb, crypto_mod, "auth",           mrb_crypto_auth,                MRB_ARGS_REQ(2));
+  mrb_define_module_function(mrb, crypto_mod, "auth_verify",    mrb_crypto_auth_verify,         MRB_ARGS_REQ(3));
+  crypto_aead_mod = mrb_define_module_under(mrb, crypto_mod, "AEAD");
+  crypto_aead_chacha20poly1305_mod = mrb_define_module_under(mrb, crypto_aead_mod, "Chacha20Poly1305");
+  mrb_define_module_function(mrb, crypto_aead_chacha20poly1305_mod, "encrypt", mrb_crypto_aead_chacha20poly1305_encrypt, MRB_ARGS_ARG(3, 1));
+  //mrb_define_module_function(mrb, crypto_aead_chacha20poly1305_mod, "decrypt", mrb_crypto_aead_chacha20poly1305_decrypt, MRB_ARGS_ARG(3, 1));
 
   if (sodium_init() == -1)
     mrb_raise(mrb, E_SODIUM_ERROR, "Cannot initialize libsodium");
@@ -289,3 +391,4 @@ void
 mrb_mruby_libsodium_gem_final(mrb_state* mrb) {
   /* finalizer */
 }
+
