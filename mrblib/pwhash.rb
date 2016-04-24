dad5f37dfc5fe90731006267ba2dcca10d2a35ee
Asmod4n <Asmod4n@users.noreply.github.com>
2016-04-24 13:49:54 +0200

    add Crypto::PwHash.salt

--- mrblib/pwhash.rb
+++ mrblib/pwhash.rb
@@ -1,5 +1,9 @@
 module Crypto
   module PwHash
+    def self.salt
+      RandomBytes.buf SALTBYTES
+    end
+
     module ScryptSalsa208SHA256
       def self.salt
         RandomBytes.buf SALTBYTES
