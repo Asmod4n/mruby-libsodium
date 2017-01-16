b37469452ca84fa7db963410290670f11585afdf
Asmod4n <Asmod4n@users.noreply.github.com>
2017-01-16 09:54:27 +0100

    update to latest libsodium stable

--- include/mruby/sodium.h
+++ include/mruby/sodium.h
@@ -1,21 +1,17 @@
 #ifndef MRUBY_SODIUM_H
 #define MRUBY_SODIUM_H
 
+#include <mruby.h>
+
 #ifdef MRB_INT16
 # error MRB_INT16 is too small for mruby-libsodium.
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <mruby.h>
+MRB_BEGIN_DECL
 
 #define E_SODIUM_ERROR (mrb_class_get_under(mrb, mrb_module_get(mrb, "Sodium"), "Error"))
 #define E_CRYPTO_ERROR (mrb_class_get_under(mrb, mrb_module_get(mrb, "Crypto"), "Error"))
 
-#ifdef __cplusplus
-}
-#endif
+MRB_END_DECL
 
 #endif
